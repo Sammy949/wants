4ea63fb4ee63aebe75341bc5ec7c56cf1712cc59
SamY <urahsamuel0202@gmail.com>
2026-09-18 02:49:37 +0100

    feat: add @expo/vector-icons dependency and restructure app layout

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,15 +1,10 @@
 import { Tabs } from "expo-router";
-import { StyleSheet } from "react-native";
 
-const _layout = () => {
+export default function TabLayout() {
   return (
     <Tabs>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
       <Tabs.Screen name="about" options={{ title: "About" }} />
     </Tabs>
   );
-};
-
-export default _layout;
-
-const styles = StyleSheet.create({});
+}
